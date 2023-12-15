c9f0a53bcb9fa857b34d2184e686d913d8eb67fe
Sellsword9 <yerayromeroortiz@gmail.com>
2023-12-15 21:24:04 +0100

    Normalization deletion

--- view/register.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "<h1>Registrarse</h1>";
-echo "<h3>Ya tienes una cuenta? <a href='index.php'>Inicia sesión</a></h3>";
-echo '<form method="post" action="index.php">
-            <input type="text" name="newusername" placeholder="Nombre de usuario" required><br>
-            <input type="password" name="newpassword" placeholder="Contraseña" required><br>
-            <input type="submit" value="Registrarse">
-            </form>';
